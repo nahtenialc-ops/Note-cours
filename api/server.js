19ae72342d20e24ad2ee338777b21ac276e68544
nahtenialc-ops <nahtenialc@gmail.com>
2026-09-21 13:35:02 +0000

    feat: donnees en memoire + route GET liste

--- api/server.js
+++ api/server.js
@@ -1,6 +1,12 @@
  const express = require("express");
 const app = express();
 
+let produits = [
+  { id: 1, nom: "clair obscur", prix: 39.9 },
+  { id: 2, nom: "Escape the backrooms", prix: 9.49 },
+  { id: 3, nom: "Ecran", prix: 120 }
+];
+
 // Route de test : GET /
 app.get("/", (req, res) => {
   res.json({ message: "Mon API fonctionne" });
@@ -9,4 +15,9 @@ app.get("/", (req, res) => {
 // On demarre le serveur sur le port 3000
 app.listen(3000, () => {
   console.log("Serveur sur http://localhost:3000");
+});
+
+// GET /produits -> renvoie tout le tableau
+app.get("/produits", (req, res) => {
+  res.json(produits);
 });
